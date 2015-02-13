5d84d4da11d4b56c89349e22daf0f402bf81d450
giovanni325 <d14125333@mydit.ie>
2015-02-13 15:04:12 +0000

    Event cause class

--- src/main/java/com/group7/sclub/EventCause.java
+++ src/main/java/com/group7/sclub/EventCause.java
@@ -1,5 +1,62 @@
 package com.group7.sclub;
 
 public class EventCause {
+	
+	import javax.persistence.Embeddable;
+	import javax.persistence.Entity; 
+	import javax.persistence.Id;
+	import javax.persistence.IdClass;
+	import javax.persistence.Table;
+	
+	/**
+	 * This class describe the
+	 * failure cause within the Event Type.
+	 * 
+	 * @author giovanni
+	 */
+	
+	@Entity 
+	@Table(name="Event_Cause")
+	public class EventCause{
+		
+		
+		@Id
+		@Column(name="Cause Code")
+		private int CauseCode;
+		
+		@Id
+		@Column(name="Event Id")
+		private int eventId;
+		
+		@Column(name="Description")
+		private String discription;
+		
+		
+
+		public int getEventCode() {
+			return eventCode;
+		}
+
+		public void setEventCode(int eventCode) {
+			this.eventCode = eventCode;
+		}
+
+		public int getEventId() {
+			return eventId;
+		}
+
+		public void setEventId(int eventId) {
+			this.eventId = eventId;
+		}
+
+		public String getDiscription() {
+			return discription;
+		}
+
+		public void setDiscription(String discription) {
+			this.discription = discription;
+		}
+
+		
 
 }
