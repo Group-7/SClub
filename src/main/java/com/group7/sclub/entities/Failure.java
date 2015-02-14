d39c2b75cdcf8fa48aab8fed27a03415ca7c2d3c
gerry <gerry@Linux>
2015-02-14 12:41:08 +0000

    Failure Entity added

--- src/main/java/com/group7/sclub/entities/Failure.java
+++ src/main/java/com/group7/sclub/entities/Failure.java
@@ -1,5 +1,41 @@
 package com.group7.sclub.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Failure_Class_Table")
 public class Failure {
 
+	@Id
+	@Column(name = "Failure_Class")
+	private Integer FailureCode;
+	
+	
+	@Column(name = "Description")
+	private String description;
+
+
+	public Integer getFailureCode() {
+		return FailureCode;
+	}
+
+
+	public void setFailureCode(Integer failureCode) {
+		FailureCode = failureCode;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
 }
